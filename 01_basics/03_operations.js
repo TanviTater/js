3b2ca3276afd753dfcaea99d652deb681bd1cb30
Tanvi <147313234+TanviTater@users.noreply.github.com>
2024-03-28 10:14:28 +0000

    Operations

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,30 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) // 2 raised to power 2
+// console.log(2**3) // 2 raised to power 3
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Tanvi"
+let str3 = str1 +str2;
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log( 1+ "2")
+// console.log("1" + 2 +2)
+// console.log( 1 + 3 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2,num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
