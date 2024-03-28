8b723569473297962b87f3704b5acedb7385e645
Tanvi <147313234+TanviTater@users.noreply.github.com>
2024-03-28 09:37:27 +0000

    Container Config Done

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,50 @@
+let score1 = "33"
+// let score2 = "33abc"
+// let score3 = null
+// let score4 = undefined
+// let score5 = true
+
+console.log(typeof score1);
+// console.log(typeof(score1));
+// console.log(typeof score2);
+// console.log(typeof score3);
+// console.log(typeof score4);
+// console.log(typeof score5);
+
+let valueInNumber1 = Number(score1)
+// let valueInNumber2 = Number(score2)
+// let valueInNumber3 = Number(score3)
+// let valueInNumber4 = Number(score4)
+// let valueInNumber5 = Number(score5)
+console.log(typeof valueInNumber1);
+// console.log(typeof valueInNumber2);
+// console.log(typeof valueInNumber3);
+// console.log(typeof valueInNumber4);
+// console.log(typeof valueInNumber5);
+console.log(valueInNumber1);
+// console.log(valueInNumber2);
+// console.log(valueInNumber3);
+// console.log(valueInNumber4);
+// console.log(valueInNumber5);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof BooleanIsLoggedIn)
+console.log(BooleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "tanvi" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
