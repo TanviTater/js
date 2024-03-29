6fe076b11fe2e307857972ca47d7817adee4332f
Tanvi <147313234+TanviTater@users.noreply.github.com>
2024-03-29 09:36:56 +0000

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -7,6 +7,18 @@
 // console.log("2">1)
 // console.log("02">1)
 
-console.log(null > 0)
-console.log(null == 0)
-console.log(null >= 0)
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+//The reason is that an equility check == and comparision ><>=<= work differently
+//Comparision convert null to a number , treating it as 0,that is why nul >= 0 is true and null > 0 is false
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+
+// null and undefined or different datatypes comparision is usually avoided
+
+/** Strict Check === **/ 
+// strict check checks even the datatypes
+console.log("2" === 2)
